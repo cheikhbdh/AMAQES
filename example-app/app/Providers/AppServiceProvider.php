64b89854a61f5b609f34e12ecfb597748c5113ca
cheikhbdh <22003@supnum.mr>
2024-06-14 22:45:57 +0000

    boudiheeee7658765

--- example-app/app/Providers/AppServiceProvider.php
+++ example-app/app/Providers/AppServiceProvider.php
@@ -1,28 +1,31 @@
 <?php
 
-// App\Providers\AppServiceProvider.php
-
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
+     *
+     * @return void
      */
-    public function register(): void
+    public function register()
     {
         //
     }
 
     /**
      * Bootstrap any application services.
+     *
+     * @return void
      */
-    public function boot(): void
+    public function boot()
     {
-        Schema::defaultStringLength(191);
+        if (config('app.env') !== 'local') {
+            URL::forceScheme('https');
+        }
     }
 }
-
